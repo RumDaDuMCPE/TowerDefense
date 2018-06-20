3cd64444ae616ac54e989d2212d7a1ab7ace94c9
RoyalMCPE <coolestgamer117@gmail.com>
2018-06-20 15:31:28 -0600

    Create PlayerEvents.php

---
+++ Events/PlayerEvents.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace TowerDefense\Events;
+
+use TowerDefense\Loader;
+use pocketmine\event\Listener;
+use pocketmine\event\player\{PlayerDeathEvent, PlayerInteractEvent, PlayerMoveEvent};
+
+class PlayerEvents implements Listener {
+
+  public function onPlayerInteract(PlayerInteractEvent $event) {
+    $player = $event->getPlayer();
+    $item = $event->getItem();
+    $block = $event->getBlock();
+    if($block->getId() === 57) {
+      Loader::get()->getScheduler()->scheduleRepeatingTask(new Extraction(Loader::get(), $player), 5);
+     }
+     if(Loader::get()->isNearSigns($player->getName())) {
+      Loader::get()->sendPlayerToGame($player->getName());
+     }
+  }
+
+  public function onPlayerMove(PlayerMoveEvent $event) {
+    $player = $event->getPlayer();
+    if(Loader::get()->isInGame($player)) {
+      $name = $player->getName();
+      if(Loader::get()->isInTower($player)) {
+        Loader::get()->getScheduler()->scheduleRepeatingTask(new Warning(Loader::get(), $player), 5);
+      }
+    }
+  }
+
+  public function onPlayerDeath(PlayerDeathEvent $event) {
+    $player = $event->getPlayer();
+    if(Loader::get()->isInGame($player)) {
+      $player->setGamemode(3);
+      $player->getInventory()->clearAll();
+      $player->getArmorInventory()->clearAll();
+      //$player->getInventory()->setItem(5, $item); $item needs to be defined
+    }
+  }
+}
