a344f6ac71d125622c79825baaddecca7d827a50
RumDaDuMCPE <ameensahal321@gmail.com>
2018-06-21 01:45:05 +0300

    Update Loader.php

--- Loader.php
+++ Loader.php
@@ -14,7 +14,7 @@ use TowerDefense\Events\{
 class Loader extends PluginBase implements Listener {
 
     private static $instance;
-    
+
     public $tasks = [];
 
     public $game1 = [];
@@ -25,87 +25,93 @@ class Loader extends PluginBase implements Listener {
     public $Team_A = [];
     public $Team_B = [];
 
-  public function onEnable() {
-       self::$instance = $this;
-      $this->registerEvents();
+    public function onEnable() {
+        self::$instance = $this;
+        $this->registerEvents();
+
+    }
+
+    public static function get() {
+        return self::$instance;
+    }
+
+    public function registerEvents() {
+        foreach ([
+                     new PlayerEvents(),
+                     new EntityEvents(),
+                 ] as $event) {
+            $this->getServer()->getPluginManager()->registerEvents($event);
+        }
+    }
+
+    public function findPlayerGame(string $player) : string {
+        if (in_array($player, $this->game1)) {
+            return "game1";
+        }
+
+        if (in_array($player, $this->game2)) {
+            return "game2";
+        }
+
+        if (in_array($player, $this->game3)) {
+            return "game3";
+        }
+
+        if (in_array($player, $this->game4)) {
+            return "game4";
+        }
+
+    }
+
+    public function sendPlayerToGame(string $player, int $coords) {
+        if ($coords = /* game1 sign coords */ && count($this->game1) =< 16) {
+            $this->game1[] = $player;
+            $this->teleportPlayerToArena($player);
+
+        }
 
   }
-  
-  public static function get() {
-    return self::$instance;
-  }
+
+    public function isInAnyGame(string $player) {
+        if (in_array($player, $this->game1) || in_array($player, $this->game2) || in_array($player, $this->game3) || in_array($player, $this->game4))
+            return true;
+    }
+
+    public function isInGame(string $player) {
+        if (isInAnyGame($player)) return true;
+    }
+
+    public function getOpponents(string $player) {
+        if (in_array($player, $this->Team_A) && $this->isInTeamB($player)) {
+            return true;
+        } elseif (in_array($player, $this->Team_B) && $this->isInTeamA($player)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getAreas() {
+        $areas = new Areas($this);
+        $this->return($areas);
+    }
+
+    public function getEnemyBlock(string $player) {
+
+    }
+
+    public function isInTower(Player $player) {
+        if ($this->getAreas()->isInTower($player)) return true;
+    }
+
+    public function unscheduleTask($id) {
+        unset ($this->Tasks[$id]);
+        $this->getScheduler()->cancelTask($id);
+    }
     
-  public function registerEvents() {
-      foreach ([
-          new PlayerEvents(),
-          new EntityEvents(),
-               ] as $event) {
-          $this->getServer()->getPluginManager()->registerEvents($event);
-      }
-  }
-
-  public function findPlayerGame(string $player) : string {
-      if (in_array($player, $this->game1)) {
-          return "game1";
-      }
-
-      if (in_array($player, $this->game2)) {
-          return "game2";
-      }
-
-      if (in_array($player, $this->game3)) {
-          return "game3";
-      }
-
-      if (in_array($player, $this->game4)) {
-          return "game4";
-      }
-
-  }
-
-  public function sendPlayerToGame(string $player, int $coords) {
-      if ($coords = /* game1 sign coords */ && count($this->game1) =< 16) {
-          $this->game1[] = $player;
-          $this->teleportPlayerToArena($player);
-
-      }
-
-  }
-
-  public function isInAnyGame(string $player) {
-      if (in_array($player, $this->game1) || in_array($player, $this->game2) || in_array($player, $this->game3) || in_array($player, $this->game4))
-          return true;
-  }
-
-  public function isInGame(string $player) {
-      if (isInAnyGame($player)) return true;
-  }
-
-  public function getOpponents(string $player) {
-      if (in_array($player, $this->Team_A) && $this->isInTeamB($player)) {
-          return true;
-      } elseif (in_array($player, $this->Team_B) && $this->isInTeamA($player)) {
-          return true;
-      } else {
-          return false;
-      }
-  }
-
-  public function getAreas() {
-      $areas = new Areas($this);
-      $this->return($areas);
-  }
-
-  public function getEnemyBlock(string $player) {
-
-  }
-
-  public function isInTower(Player $player) {
-      if ($this->getAreas()->isInTower($player)) return true;
-  }
-
-  public function unscheduleTask($id) {
-      unset ($this->Tasks[$id]);
-      $this->getScheduler()->cancelTask($id);
-  }
+    public function getMessages(string $msg, Player $player) : string {
+        $messages = new api\game\Messages();
+        $message = $messages->getMessage($msg, $player);
+        return $message;
+    }
 }
