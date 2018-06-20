4a5669c29de43a73fc8b5b91c3d8455ccede523f
RumDaDuMCPE <ameensahal321@gmail.com>
2018-06-21 01:58:40 +0300

    Update Messages.php

--- api/game/Messages.php
+++ api/game/Messages.php
@@ -9,6 +9,7 @@ class messages {
     public function getMessage(string $msg, Player $player) : string {
         $messages = [
             "breach" => "&cYour tower is being breached by &6".$player->getName()."&r&c!",
+            "extraction.successul" => "&aYou have successfully extracted the block!\n&e&lMake it to your tower to complete the extraction!",
             "error" => "&4ERROR FETCHING MESSAGE!"
         ];
 
@@ -19,6 +20,9 @@ class messages {
         switch ($msg) {
             case "breach":
                 return $messages["breach"];
+                break;
+            case "extraction.successful":
+                return $messages["extraction.successful"];
                 break;
             default:
                 return $messages["error"];
