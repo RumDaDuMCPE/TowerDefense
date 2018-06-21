5eb22f1b1dd8e5b5bd6c6c6643b536050a6d1ddb
RoyalMCPE <coolestgamer117@gmail.com>
2018-06-20 21:46:19 -0600

    Healing System
    This system has been put in place to allow their teammates to health them within 15 seconds!

--- Events/EntityEvents.php
+++ Events/EntityEvents.php
@@ -5,9 +5,12 @@ namespace TowerDefense\Events;
 use TowerDefense\Loader;
 use TowerDefense\api\game\GameManager;
 use TowerDefense\api\player\PlayerManager;
+use TowerDefense\Tasks\DeathTask;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\{EntityDamageEvent, EntityDamageByEntityEvent};
 use pocketmine\Player;
+use pocketmine\utils\TextFormat as c;
+use pocketmine\entity\Entity;
 
 class EntityEvents implements Listener {
 
@@ -16,6 +19,9 @@ class EntityEvents implements Listener {
     $cause = $event->getCause();
     if($event instanceof EntityDamageByEntityEvent) {
       $damager = $event->getDamager();
+      if($entity->namedtag->hasTag("Corpse")) {
+        $event->setCancelled(true);
+      }
       if($entity instanceof Player && $damager instanceof Player) {
         $entityData = PlayerManager::get()->getPlayer($entity->getName());
         $damagerData = PlayerManager::get()->getPlayer($damager->getName());
@@ -23,6 +29,34 @@ class EntityEvents implements Listener {
           $game = GameManager::get()->getGame($damagerData->getGameId());
           if($game->getTeamManager()->getTeamByPlayer($entity) === $game->getTeamManager()->getTeamByPlayer($damager)) {
             $event->setCancelled(true);
+            if($entity->getHealth() === 1) {
+              if(!$entity->hasTag("Killed")) {
+                $nbt = Entity::createBaseNBT($entity);
+                $nbt->setTag(clone $entity->namedtag->getCompound("Skin"));
+                $human = Entity::createEntity("Human", $entity->getLevel(), $nbt);
+                $human->getDataPropertyManager()->setBlockPos($human::DATA_PLAYER_BED_POSITION, $entity);
+                $human->setPlayerFlag($human::DATA_PLAYER_FLAG_SLEEP, true);
+                $human->spawnToAll();
+                $human->despawnFrom($entity);
+                $entity->despawnFromAll();
+                $entity->setImmobile(true);
+                Loader::get()->getScheduler()->scheduleRepeatingTask(new DeathTask($entity, $human), 20);
+              } else {
+                if($damager->getInventory()->getItemInHand()->getName() === c::RESET . c::BOLD . c::GREEN . "Bandage") {
+                  $entity->setHealth(2);
+                  $item = $damager->getInventory()->getItemInHand();
+                  $item->pop();
+                  $damager->getInventory()->setItemInHand($item);
+                  foreach($entity->getLevel()->getEntities() as $human) {
+                    if($human->namedtag->hasTag("Corpse") && $human->namedtag->getTag("Corpse") === $entity->getName()) {
+                      $human->close()
+                    }
+                  }
+                  $entity->setImmobile(false);
+                  $entity->spawnToAll();
+                }
+              }
+            }
           }
         }
       }
