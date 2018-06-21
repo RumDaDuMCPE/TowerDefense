a477bd3dbe5a26248a8c97d9766226dc93dd8223
RumDaDuMCPE <ameensahal321@gmail.com>
2018-06-21 20:34:20 +0300

    Create Areas.php

---
+++ Areas.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TowerDefense;
+
+use pocketmine\Player;
+
+class Areas {
+    public function isInTower(Player $player) {
+        $px = $player->x;
+        $pz = $player->z;
+        $x1 = // TODO
+        $x2 = // TODO
+        $z1 = // TODO
+        $z2 = // TODO
+        if () {
+            // TODO
+        }
+    }
+}
