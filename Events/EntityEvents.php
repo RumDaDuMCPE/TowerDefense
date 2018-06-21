f2026adae65117087704a6ea651b492dc64fb5f5
RoyalMCPE <coolestgamer117@gmail.com>
2018-06-20 20:53:37 -0600

    Add checks too detect if both players are on the same team

--- Events/EntityEvents.php
+++ Events/EntityEvents.php
@@ -3,6 +3,8 @@
 namespace TowerDefense\Events;
 
 use TowerDefense\Loader;
+use TowerDefense\api\game\GameManager;
+use TowerDefense\api\player\PlayerManager;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\{EntityDamageEvent, EntityDamageByEntityEvent};
 use pocketmine\Player;
@@ -14,8 +16,15 @@ class EntityEvents implements Listener {
     $cause = $event->getCause();
     if($event instanceof EntityDamageByEntityEvent) {
       $damager = $event->getDamager();
-      if($entity instanceof Player && $damager instanceof Player) { // TODO: Check if both players are in the same team and then cancel.
-        $event->setCancelled(true);
+      if($entity instanceof Player && $damager instanceof Player) {
+        $entityData = PlayerManager::get()->getPlayer($entity->getName());
+        $damagerData = PlayerManager::get()->getPlayer($damager->getName());
+        if($entityData->isPlaying() && $damagerData->isPlaying()) {
+          $game = GameManager::get()->getGame($damagerData->getGameId());
+          if($game->getTeamManager()->getTeamByPlayer($entity) === $game->getTeamManager()->getTeamByPlayer($damager)) {
+            $event->setCancelled(true);
+          }
+        }
       }
     }
   }
