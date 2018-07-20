f1b40d3867483f31362601b1fb741a44567ca9e7
RoyalMCPE <coolestgamer117@gmail.com>
2018-07-20 11:58:30 -0600

    Create GameManager.php

---
+++ api/game/GameManager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TowerDefense\api\game;
+
+use TowerDefense\api\game\entities\GameMap;
+
+class GameManager {
+
+    private static $instance;
+    
+    private $games = [];
+    
+    public function __construst() {
+        self::$instance = $this;
+    }
+    
+    public static function get() {
+        return self::$instance;
+    }
+    
+    public function getGame(string $id): Game {
+        return isset($this->games[$id]) ? $this->games[$id] : null;
+    }
+    
+    public function getGames(): array {
+        return $this->games;
+    }
+    
+    public function createGame(string $id, GameMap $map): Game {
+        $game = new Game($id, [], [], $map);
+        $this->games[$id] = $game;
+        return $game;
+    }
+    
+    //TODO
+    public function loadGamesFromSigns() {}
+}
