3e94d522176c0ba0eedd8dd7d20aff482f1ff387
RoyalMCPE <coolestgamer117@gmail.com>
2019-04-12 13:20:45 -0600

    Fix formatting

--- Utils.php
+++ Utils.php
@@ -6,10 +6,10 @@ class Utils {
 
     public static generateID(array &$pSource): string {
         do {
-			    $id = implode("-", array_values(array_map(function ($el) {
-				    return implode("", $el);
-			    }, array_chunk(str_split(md5(mt_rand(0, PHP_INT_MAX)) . md5(mt_rand(0, PHP_INT_MAX))), 4))));
-		    } while (isset($pSource[$id]));
-		    return substr($id, 0, 4 * 4 + 3);
+		$id = implode("-", array_values(array_map(function ($el) {
+			return implode("", $el);
+		}, array_chunk(str_split(md5(mt_rand(0, PHP_INT_MAX)) . md5(mt_rand(0, PHP_INT_MAX))), 4))));
+	} while (isset($pSource[$id]));
+	return substr($id, 0, 4 * 4 + 3);
     }
 }
